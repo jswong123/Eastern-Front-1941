7c4bdb351bf7d967d0aef4e5a2c9ed8198da59bf
jswong123 <wrx15058181135@163.com>
2026-09-23 13:04:47 +1000

    Implement MapFeatures class with various methods

--- src/MapFeatures.js
+++ src/MapFeatures.js
@@ -1,1 +1,43 @@
+export class MapFeatures {
 
+    constructor() {
+        this.rivers = [];
+        this.roads = [];
+        this.railways = [];
+        this.settlements = [];
+    }
+
+    addRiver(name, points, options = {}) {
+        this.rivers.push({
+            name,
+            points,
+            width: options.width ?? 5
+        });
+    }
+
+    addRoad(name, points, options = {}) {
+        this.roads.push({
+            name,
+            points,
+            importance: options.importance ?? "main"
+        });
+    }
+
+    addRailway(name, points) {
+        this.railways.push({
+            name,
+            points
+        });
+    }
+
+    addSettlement(data) {
+        this.settlements.push({
+            id: data.id,
+            name: data.name,
+            nameZh: data.nameZh,
+            q: data.q,
+            r: data.r,
+            type: data.type ?? "town"
+        });
+    }
+}
