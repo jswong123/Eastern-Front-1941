995caaa2e95d3e82b8ab06f0daa7c1028dc25b90
jswong123 <wrx15058181135@163.com>
2026-09-23 12:43:28 +1000

    Add hexToPixel and drawHexPath functions

--- src/Hex.js
+++ src/Hex.js
@@ -1,1 +1,45 @@
+export const SQRT3 = Math.sqrt(3);
 
+
+export function hexToPixel(q, r, size) {
+
+    return {
+        x: size * SQRT3 * (q + r / 2),
+        y: size * 1.5 * r
+    };
+
+}
+
+
+export function drawHexPath(ctx, x, y, size) {
+
+    ctx.beginPath();
+
+    for (let i = 0; i < 6; i++) {
+
+        const angle =
+            Math.PI / 180 *
+            (60 * i - 30);
+
+        const px =
+            x + size * Math.cos(angle);
+
+        const py =
+            y + size * Math.sin(angle);
+
+
+        if (i === 0) {
+
+            ctx.moveTo(px, py);
+
+        } else {
+
+            ctx.lineTo(px, py);
+
+        }
+
+    }
+
+    ctx.closePath();
+
+}
